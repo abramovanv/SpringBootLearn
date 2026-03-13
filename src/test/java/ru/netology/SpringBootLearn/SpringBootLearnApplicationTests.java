3973307cdcd69dcdf3ddf276a5095831d5e84888
abramovanv <abramov_anv@rambler.ru>
2026-03-13 14:05:26 +0300

    second fix test commit

--- src/test/java/ru/netology/SpringBootLearn/SpringBootLearnApplicationTests.java
+++ src/test/java/ru/netology/SpringBootLearn/SpringBootLearnApplicationTests.java
@@ -1,43 +1,39 @@
 package ru.netology.SpringBootLearn;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootLearnApplicationTests {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Container
     private static final GenericContainer<?> myAppDev = new GenericContainer<>("devapp")
             .withExposedPorts(8080);
-
+    @Container
     private static final GenericContainer<?> myAppProm = new GenericContainer<>("prodapp")
             .withExposedPorts(8081);
-
-    @BeforeAll
-    static void setUp() {
-        myAppDev.start();
-        myAppProm.start();
-    }
 
     @Test
     @DisplayName("Проверка стенда Dev")
     void contextLoadsDev() {
         ResponseEntity<String> entityFirst = restTemplate.getForEntity("http://localhost:" + myAppDev.getMappedPort(8080) + "/profile", String.class);
-		Assertions.assertEquals("Current profile is dev", entityFirst.getBody().toString());
+        Assertions.assertEquals("Current profile is dev", entityFirst.getBody().toString());
     }
 
     @Test
     @DisplayName("Проверка стенда Prom")
     void contextLoadsProm() {
         ResponseEntity<String> entitySecond = restTemplate.getForEntity("http://localhost:" + myAppProm.getMappedPort(8081) + "/profile", String.class);
-		Assertions.assertEquals("Current profile is production", entitySecond.getBody().toString());
+        Assertions.assertEquals("Current profile is production", entitySecond.getBody().toString());
     }
 }
